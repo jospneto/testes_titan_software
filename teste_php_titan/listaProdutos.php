0f6f26a917ad04e9a7b7a699d61c7970134958bf
jospneto <josenetopereira380@gmail.com>
2022-07-12 16:58:24 -0300

    Atualizações

--- teste_php_titan/listaProdutos.php
+++ teste_php_titan/listaProdutos.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styleTabela.css">
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Lato:wght@100&family=Martel+Sans:wght@800&family=Montserrat:wght@100&family=Oswald:wght@200&family=Pacifico&display=swap" rel="stylesheet">
     <title>Lista de produtos</title>
 </head>
@@ -31,16 +31,16 @@
                         echo '<td>'.$codigoProd =  $produto['idprod'].'</td>';
                         echo '<td>'.$nome =  $produto['nome'].'</td>';
                         echo '<td>'.$cor =  $produto['cor'].'</td>';
-                        echo '<td> R$'.number_format($preco =  $produto['preco'], 2, ',', '.').'</td>';
-                        if(strtoupper($cor) === 'AZUL' or strtoupper($cor) === 'VERMELHO'){
+                        echo '<td> R$'.number_format($preco = $produto['preco'], 2, ',', '.').'</td>';
+                        if(strtoupper($cor) == 'AZUL' or strtoupper($cor) == 'VERMELHO'){
                             $desconto = $preco * 0.20;
                             $descontoFinal = $preco - $desconto;
                             echo '<td> R$'.$descontoFinal.'</td>';
-                        }else if(strtoupper($cor) === 'AMARELO'){
+                        }else if(strtoupper($cor) == 'AMARELO'){
                             $desconto = $preco * 0.10;
                             $descontoFinal = $preco - $desconto;
                             echo '<td> R$'.$descontoFinal.'</td>';
-                        }else if(strtoupper($cor) === 'AMARELO' and $preco > 50){
+                        }else if(strtoupper($cor) == 'AMARELO' and $preco > 50){
                             $desconto = $preco * 0.05;
                             $descontoFinal = $preco - $desconto;
                             echo '<td> R$'.number_format($descontoFinal, 2, ',', '.').'</td>';
